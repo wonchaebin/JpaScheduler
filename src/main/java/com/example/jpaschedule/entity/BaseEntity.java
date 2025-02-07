0235eedac5918547234e63c810d9591caab23f35
wonchaebin <usan119@naver.com>
2025-02-07 19:08:49 +0900

    initial commit

---
+++ src/main/java/com/example/jpaschedule/entity/BaseEntity.java
@@ -0,0 +1,4 @@
+package com.example.jpaschedule.entity;
+
+public class BaseEntity {
+}
---
+++ src/test/java/com/example/jpaschedule/JpascheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpascheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
